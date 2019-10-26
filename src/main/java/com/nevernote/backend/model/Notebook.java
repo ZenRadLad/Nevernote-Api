34edd6b525bc3beef4d0fba581d1d977c07ca9eb
Hamza <175264@supinfo.com>
2019-10-26 18:06:09 +0200

    deleting notebook will delete all of its notes

--- src/main/java/com/nevernote/backend/model/Notebook.java
+++ src/main/java/com/nevernote/backend/model/Notebook.java
@@ -1,8 +1,11 @@
 package com.nevernote.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.Set;
 
 @Entity
 @Table(name = "note_book")
@@ -19,6 +22,10 @@ public class Notebook {
 
     @Column(name="last_modification_date")
     private Date lastModificationDate;
+
+    @JsonIgnore
+    @OneToMany(mappedBy="notebook", orphanRemoval = true, cascade = CascadeType.PERSIST)
+    private Set<Note> notes;
 
     public Notebook() {
     }
@@ -58,6 +65,14 @@ public class Notebook {
 
     public void setLastModificationDate(Date lastModificationDate) {
         this.lastModificationDate = lastModificationDate;
+    }
+
+    public Set<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Set<Note> notes) {
+        this.notes = notes;
     }
 
     @Override
